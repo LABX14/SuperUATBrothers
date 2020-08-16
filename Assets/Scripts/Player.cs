e0df0c436e08e2da20c5b875851cf0cc3b92778e
Kaulana Lee <Kaulanalee12@gmail.com>
2020-08-15 18:58:41 -0700

    Gave my character more animations and a player controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,20 @@ public class Player : MonoBehaviour
 
     public float speed = 3f;
     public float jumpForce = 5f;
-    public int maxJumps = 1;
-    public float height = 1.1f;
+    public int currentJumps;
+    public int maxJumps = 2;
+    public float height = 4.1f;
 
     private Rigidbody2D rigidBody;
     private SpriteRenderer sprite;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        currentJumps = maxJumps;
     }
 
     // Update is called once per frame
@@ -24,11 +28,21 @@ public class Player : MonoBehaviour
     {
         // This allows my sprite to move 
         float xMovement = Input.GetAxis("Horizontal") * speed;
+        animator.SetFloat("xMove", Mathf.Abs(xMovement));
 
         rigidBody.velocity = new Vector2(xMovement, rigidBody.velocity.y);
 
+        if (rigidBody.velocity.x != 0)
+        {
+            animator.Play("PlayerWalk");
+        }
+        else
+        {
+            animator.Play("PlayerIdle");
+        }
+
         // This allows my sprite to stay facing in the direction they were moving
-        if(rigidBody.velocity.x > 0)
+        if (rigidBody.velocity.x > 0)
         {
             sprite.flipX = false;
         }
@@ -40,17 +54,33 @@ public class Player : MonoBehaviour
         // This allows my sprite to jump
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if (IsGrounded())
+            {
+                currentJumps = maxJumps;
+            }
+            if (currentJumps > 0)
+            {
+                Jump();
+            }
+        }
     }
-
     void Jump()
     {
-        rigidBody.AddForce(Vector2.up * jumpForce);
+        currentJumps --;
+        //rigidBody.AddForce(Vector2.up * jumpForce);
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
     }
 
     bool IsGrounded()
     {
         RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, Vector2.down, (height / 2f) + 0.1f);
-        return (hitinfo.collider != null);
+        bool grounded = (hitinfo.collider != null);
+        animator.SetBool("IsGrounded", grounded);
+        return grounded;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IsGrounded();
     }
 }
